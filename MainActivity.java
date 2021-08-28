35789607414085601284526eafc133ae49a6849b
JackCotter <79673714+JackCotter@users.noreply.github.com>
2021-08-28 07:48:19 -0700

    Update MainActivity.java
    added on and off button

--- MainActivity.java
+++ MainActivity.java
@@ -1,16 +1,12 @@
-package com.example.newapp;
+package com.example.screenfilter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.content.Context;
-
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,43 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    public void disable(View v){
-        findViewById(R.id.button).setEnabled(false);
-        ((Button)findViewById(R.id.button)).setText("disabled?");
-    }
-
-    public void handleText(View v){
-        TextView t = findViewById(R.id.inputtext);
-        String input = t.getText().toString();
-        Log.d("info", input);
 
     }
+    int STATE = 0;
 
-}
 
-public class SharedMemory{
-    private SharedPreferences mSharedPreferences;
 
-    public SharedMemory(Context context){
-        mSharedPreferences = context.getSharedPreferences("SCREEN_FILTER_PREF", Context.MODE_PRIVATE)
-    }
-    private int getValue(String prop, int def) {
-        return mSharedPreferences.getInt(prop, def);
-    }
-
-    public int getAlpha(){
-        return getValue("alpha", 0x33);
-    }
-    private void setValue(String value, int v){
-        mSharedPreferences.edit().putInt(value, v).apply();
-    }
-    public void setAlpha(int val){
-        setValue("alpha", val);
-    }
-
-    public int getColor(){
-        return Color.argb(getAlpha(), 1, 1, 1)
+    public void turnon(View view){
+        if(STATE == 0) {
+            view = findViewById(R.id.MyLayout);
+            view.setBackgroundColor(Color.rgb(200, 200, 200));
+            STATE = 1;
+        }else if (STATE == 1){
+            view = findViewById(R.id.MyLayout);
+            view.setBackgroundColor(android.R.color.background_light);
+            STATE = 0;
+        }
     }
 }
